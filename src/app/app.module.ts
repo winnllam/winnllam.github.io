2d5421ff7376c0fcd074a4d30def0a2df3adcbf6
winnllam <winnieee.lam00@gmail.com>
2020-05-18 22:08:27 -0400

    moved data to json files and initial media queries for projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { LayoutModule } from '@angular/cdk/layout';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { ActivitiesComponent } from './activities/activities.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    LayoutModule,
     FontAwesomeModule,
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { ObjectUnsubscribedError } from 'rxjs';
+
+import resume from './json/resume.json';
+import project from './json/project.json';
+import activity from './json/activity.json';
 
 @Component({
   selector: 'app-home',
@@ -20,73 +23,9 @@ export class HomeComponent implements OnInit {
     'An aspiring computer science student with a passion for aesthetics, creation and computers.';
 
   constructor() {
-    this.resume = [
-      {
-        role: 'Front-End Developer',
-        company: 'LIVE Competition',
-        duration: 'April 2020 - Present',
-      },
-      {
-        role: 'Developer Co-op',
-        company: 'Royal Bank of Canada (RBC)',
-        duration: 'January 2020 - April 2020',
-      },
-      {
-        role: 'Computer Science Teaching Assistant',
-        company: 'University of Toronto Scarborough',
-        duration: 'September 2019 - December 2019',
-      },
-      {
-        role: 'Student',
-        company: "Google's Computer Science Summer Institute",
-        duration: 'July 2018',
-      },
-    ];
-
-    this.project = [
-      {
-        name: 'Streamline',
-        oneLiner:
-          'an app that makes the process easier for international students',
-        bullets: [
-          'Won 3rd place for RBC Innovation Challenge in a team with 4 fellow co-ops',
-          'Built an application that streamlines the experience for international students coming to Canada with a currency converter widget, step-by-step guide for permits, and map marking and locating',
-        ],
-      },
-      {
-        name: 'Chat Bot Box',
-        oneLiner: 'a Chat Box with Dialogflow Agents',
-        bullets: [
-          'Developed a chat box UI to enable users to chat with agents outside of the Dialogflow console',
-          'Retrieved responses from Dialogflow API for a complete conversation flow',
-        ],
-      },
-      {
-        name: 'Pineapple',
-        oneLiner: 'an eCommerce Android Application',
-        bullets: [
-          'An eCommerce android application using Java, SQLite, and Android Studio',
-          'Stores and retrieves purchases and accounts for each user, and displayed the app’s shopping and cart content in a lucid and user-friendly manner',
-        ],
-      },
-    ];
-
-    this.activity = [
-      {
-        role: 'Student Ambassador (Tour Guide)',
-        bullets: [
-          'Guided prospective students and parents through campus in groups of 5 or more people for 45-minute tours',
-          'Clarified inquiries about campus life and education to assist in making post-secondary decisions',
-        ],
-      },
-      {
-        role: 'WiCSM Mentor',
-        bullets: [
-          'Mentored first year CMS students one-on-one in many different aspects to help students feel a sense of community within the program',
-          'Provided resume help, study habits, campus tours, and gave general tips',
-        ],
-      },
-    ];
+    this.resume = resume;
+    this.project = project;
+    this.activity = activity;
   }
 
   ngOnInit() {
