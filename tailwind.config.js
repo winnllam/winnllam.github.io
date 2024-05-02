cd3e1feee13a240649d78f32234479fcd0eff704
Winn <winnieee.lam00@gmail.com>
2024-05-02 00:35:28 -0400

    added theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      gray: "#485665",
+      metal: "#1e3231",
+      lavender: "#8e7c93",
+      pink: "#d0a5c0",
+      orchid: "#f6c0d0",
+    },
     extend: {},
   },
   plugins: [],
