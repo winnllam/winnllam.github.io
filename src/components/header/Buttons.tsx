7e57329b00914235f281cfca86caf0b751c8d92f
Winn <winnieee.lam00@gmail.com>
2024-05-03 14:08:23 -0400

    Added link to resume

--- src/components/header/Buttons.tsx
+++ src/components/header/Buttons.tsx
@@ -1,7 +1,11 @@
 function Buttons() {
+  const resumeUrl =
+    "https://drive.google.com/file/d/1FO4ty81THO92dGWc4gBToEf70l2ZW55Y/view?usp=sharing";
   return (
     <div className="container flex justify-center">
-      <button className="btn btn-orchid m-2">Resume</button>
+      <a href={resumeUrl} target="_blank" rel="noreferrer">
+        <button className="btn btn-orchid m-2">Resume</button>
+      </a>
       <button className="btn btn-orchid-outline m-2">Contact</button>
     </div>
   );
