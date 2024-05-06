76f4515f8d64f0a9a2b3fc87e4bfc135af30e417
Winn <winnieee.lam00@gmail.com>
2024-05-06 15:17:24 -0400

    Added fotter

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,42 @@
 import "./Footer.css";
+import { BsLinkedin, BsGithub, BsBrushFill } from "react-icons/bs";
 
 function Footer() {
-  return <></>;
+  return (
+    <footer id="footer" className="bg-pink text-black z-50">
+      <div className="container p-20 text-center font-mono">
+        <h2>Winnie Lam</h2>
+        <h4>winnie.lam540@gmail.com</h4>
+        <div className="width-fit mt-10 mb-10 text-3xl flex flex-row gap-10 justify-center">
+          <a
+            href="https://www.linkedin.com/in/winnllam"
+            target="_blank"
+            rel="noreferrer"
+            className="social"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            href="https://github.com/winnllam"
+            target="_blank"
+            rel="noreferrer"
+            className="social"
+          >
+            <BsGithub />
+          </a>
+          <a
+            href="https://www.artstation.com/llunamous"
+            target="_blank"
+            rel="noreferrer"
+            className="social"
+          >
+            <BsBrushFill />
+          </a>
+        </div>
+        <p>Created with ❤️ by Winnie Lam</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
