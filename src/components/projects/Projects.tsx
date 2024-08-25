9097258a57837b974abb3a32dfbbc7c14b2d3e62
Winn <winnieee.lam00@gmail.com>
2024-08-24 23:59:22 -0400

    Completed project section and migrated old projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,7 +1,46 @@
 import "./Projects.css";
+import { PROJECTS as data } from "./projectList";
+import { BsGithub } from "react-icons/bs";
+
+declare const require: {
+  context: (
+    directory: string,
+    useSubdirectories: boolean,
+    regExp: RegExp
+  ) => {
+    keys: () => string[];
+    (id: string): string;
+  };
+};
 
 function Projects() {
-  return <></>;
+  const images = require.context("../../assets/projects", false, /\.png$/);
+
+  return (
+    <div id="projects">
+      <h2 className="text-center font-mono m-10 text-orchid">Projects</h2>
+      <div className="container px-12 sm:px-36 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 font-mono mb-10">
+        {data.map((item: any) => (
+          <div key={item.title} className="box flex flex-col justify-between">
+            <div className="flex flex-col flex-grow">
+              <h3 className="text-center mb-2">{item.title}</h3>
+              <img
+                src={images(`./${item.title.replace(/\s+/g, "_")}.png`)}
+                alt={item.title}
+                className="w-full object-contain max-h-48"
+              ></img>
+              <p className="mt-4 mb-2">{item.description}</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <a href={item.link} target="_blank" rel="noreferrer">
+                <BsGithub className="text-3xl" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
---
+++ src/components/projects/projectList.js
@@ -0,0 +1,32 @@
+export const PROJECTS = [
+  {
+    title: "1Secret",
+    description:
+      "Working together with mentors from 1Password for deCODE Hackathon; 1Secret is a web app that lets you share temporary secret links for sensitive information from 1Password vault. Share your passwords securely with time-sensitive, shareable links.",
+    link: "https://github.com/winnllam/1Secret",
+  },
+  {
+    title: "Find Dining",
+    description:
+      "Chosen as the winning project by the City of Toronto and the BRIDGE UTSC, a food ordering app that allows restaurant owners to share their stories and have online presence during the hard times of COVID-19.",
+    link: "https://github.com/winnllam/team_08-project",
+  },
+  {
+    title: "Streamline",
+    description:
+      "RBC Innovation Challenge winner! (3rd place) An app to help international students navigate finances and permits, local deals, and streamline banking in a new country using a currency convertor and link to local financial advisors around campus.",
+    link: "https://github.com/winnllam/team_08-project",
+  },
+  {
+    title: "Doodle Jump",
+    description:
+      "A functional Doodle Jump game done in MIPS Assembly with different functionality and features similar to the original game.",
+    link: "https://github.com/winnllam/doodle-jump",
+  },
+  {
+    title: "Pineapple",
+    description:
+      "A functional eCommerce mobile android application selling pineapples. This application includes owner, manager, and customer roles.",
+    link: "https://github.com/winnllam/Pineapple",
+  },
+];
