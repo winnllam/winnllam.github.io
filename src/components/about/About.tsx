663abaa4399a374ad7f428421659f937d90a4da6
Winn <winnieee.lam00@gmail.com>
2024-08-25 21:27:47 -0400

    Added skills and tools icons

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,42 @@
 import "./About.css";
+import { FC, useState } from "react";
+import { SKILLS as skills, TOOLS as tools } from "./tools";
 import { GiGraduateCap } from "react-icons/gi";
 import { BsPeopleFill, BsFillTrophyFill } from "react-icons/bs";
+import * as DiIcons from "react-icons/di";
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
+
+const Icons = { ...DiIcons, ...FaIcons, ...SiIcons };
+
+interface IconProps {
+  name: string;
+  icon: string;
+  type: string;
+}
+
+const IconCard: FC<IconProps> = ({ name, icon, type }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const IconComponent = Icons[`${type}${icon}` as keyof typeof Icons];
+
+  if (!IconComponent) {
+    return null;
+  }
+
+  return (
+    <div
+      className="box-outline-orchid m-2"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered ? (
+        <span className="text-lg">{name}</span>
+      ) : (
+        <IconComponent className="text-6xl" />
+      )}
+    </div>
+  );
+};
 
 function About() {
   return (
@@ -46,6 +82,22 @@ function About() {
           leadership standpoint. I am excited to apply my skills and knowledge
           to new and exciting opportunities.
         </p>
+      </div>
+      <div className="container px-12 sm:px-36 flex flex-col items-center font-mono">
+        <h3>Skill Set</h3>
+        <div className="flex flex-wrap justify-center items-center w-full">
+          {skills.map((item) => (
+            <IconCard name={item.name} icon={item.icon} type={item.type} />
+          ))}
+        </div>
+      </div>
+      <div className="container px-12 my-10 sm:px-36 flex flex-col items-center font-mono">
+        <h3>Tool Set</h3>
+        <div className="flex flex-wrap justify-center items-center w-full">
+          {tools.map((item) => (
+            <IconCard name={item.name} icon={item.icon} type={item.type} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/about/tools.js
@@ -0,0 +1,26 @@
+export const SKILLS = [
+  { name: "NodeJS", icon: "Nodejs", type: "Di" },
+  { name: "React", icon: "React", type: "Di" },
+  { name: "Angular", icon: "AngularSimple", type: "Di" },
+  { name: "Python", icon: "Python", type: "Di" },
+  { name: "JavaScript", icon: "JsBadge", type: "Di" },
+  { name: "TypeScript", icon: "Typescript", type: "Si" },
+  { name: "HTML", icon: "Html5", type: "Di" },
+  { name: "CSS", icon: "Css3", type: "Di" },
+  { name: "Django", icon: "Django", type: "Di" },
+  { name: "Flask", icon: "Flask", type: "Si" },
+  { name: "Electron", icon: "Electron", type: "Si" },
+  { name: "MongoDB", icon: "Mongodb", type: "Di" },
+  { name: "MySQL", icon: "Mysql", type: "Di" },
+  { name: "PostgreSQL", icon: "Postgresql", type: "Di" },
+  { name: "SQLite", icon: "Sqllite", type: "Di" },
+];
+
+export const TOOLS = [
+  { name: "Terminal", icon: "Terminal", type: "Di" },
+  { name: "GitHub", icon: "Github", type: "Fa" },
+  { name: "AWS", icon: "Aws", type: "Fa" },
+  { name: "Docker", icon: "Docker", type: "Fa" },
+  { name: "Figma", icon: "Figma", type: "Fa" },
+  { name: "Jira", icon: "Jira", type: "Fa" },
+];
